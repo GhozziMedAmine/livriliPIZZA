8ffb52694d54865c45680ae468bda2f6bfaea7ac
Ayoub Ghozzi <ghozziayoub@gmail.com>
2020-12-17 02:12:58 +0100

    fin_homepage

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//custom JS
+
+$(document).ready(function(){
+    //owl banner slider
+    $("#banner-slider.owl-carousel").owlCarousel({
+
+        items: 1,
+        loop: true,
+        autoplay: true
+    });
+
+
+});
